321276583e5a0e3e8229639f56a96e8803610222
plungerman <plungerman@gmail.com>
2018-01-18 16:03:40 -0600

    new base template extended from new bootstrap admin dashboard

--- djpagan/billing/views.py
+++ djpagan/billing/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
 
 from djpagan.billing.forms import SearchTransactionForm
 from djpagan.billing.sql import TRANSACTIONS
 from djpagan.core.utils import get_objects
 
-#from djzbar.decorators.auth import portal_auth_required
+from djzbar.decorators.auth import portal_auth_required
 
 
+@portal_auth_required(
+    'StudentAccounts', 'DJPAGAN_AUTH', reverse_lazy('access_denied')
+)
 def search(request, tipo=None):
 
     objects = None
