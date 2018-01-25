79518fab6b9960b433e249bd794153895a0461f2
plungerman <plungerman@gmail.com>
2018-01-25 14:18:16 -0600

    new search infrastructure for all student transactions and for all transactions by cheque number

--- djpagan/core/views.py
+++ djpagan/core/views.py
@@ -13,15 +13,17 @@ from djzbar.decorators.auth import portal_auth_required
 )
 def home(request):
 
-    form_journal = SearchJournalForm(prefix='journal')
     form_bridged = SearchBridgedForm(prefix='bridged')
+    form_cheque = SearchChequeForm(prefix='cheque')
+    form_journal = SearchJournalForm(prefix='journal')
     form_transaction = SearchTransactionForm(prefix='transaction')
 
     return render(
         request, 'core/home.html',
         {
-            'form_journal':form_journal,
             'form_bridged':form_bridged,
+            'form_cheque':form_cheque,
+            'form_journal':form_journal,
             'form_transaction':form_transaction
         }
     )
