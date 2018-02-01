e473a0ccbf9ccd445e2290431be3883ef7db2a60
plungerman <plungerman@gmail.com>
2018-02-01 14:09:16 -0600

    finalizing the unit tests for core

--- djpagan/core/tests.py
+++ djpagan/core/tests.py
@@ -1,60 +1,88 @@
-from django.conf import settings
-from django.test import TestCase
 from django.test import Client
+from django.test import TestCase
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import Group
 
-USERNAME = settings.TEST_USERNAME
-PASSWORD = settings.TEST_PASSWORD
+D = "-------------------------------------------------------------------------"
 
 
 class CoreViewsTestCase(TestCase):
 
     def setUp(self):
+
+        self.username = settings.TEST_USERNAME
+        self.email = settings.TEST_EMAIL
+        self.password = settings.TEST_PASSWORD
+        self.user = User.objects.create_user(
+            self.username, self.email, self.password
+        )
+
+        # add to student accounts group
+        sa = Group.objects.create(name=settings.MANAGER_GROUP)
+        sa.user_set.add(self.user)
+        # cred dict
         self.credentials = {
-            'username': USERNAME,
-            'password': PASSWORD
+            'username': self.username,
+            'password': self.password
         }
 
-    def test_signin(self):
-        print "cred:"
-        print self.credentials
-        # attempt to sign in
-        signin = self.client.post(
-            reverse('auth_login'),
-            self.credentials, follow=True
-        )
-        print reverse('auth_login')
-        print "status code: {}".format(signin.status_code)
-        print "post login status = {}".format(signin.context['user'].is_active)
-
-        session = self.client.session
-        print session
-
-        login = self.client.login(username=USERNAME, password=PASSWORD)
-        print "client login status = {}".format(login)
 
     def test_home(self):
+        print "\n"
+        print "Home Page Test"
+        print D
+        earl = reverse('home')
+        print earl
         # get home page
-        response = self.client.get(reverse('home'))
-        # redirect to sign in page
+        response = self.client.get(earl)
         self.assertEqual(response.status_code, 302)
-        print "status code: {}".format(response.status_code)
+        # redirect to sign in page
         print "redirect to sign in at {}".format(response['location'])
 
-        # attempt to sign in with post
-        signin = self.client.post(
-            response['location'],
-            self.credentials, follow=True
-        )
-        print "status code: {}".format(signin.status_code)
-        #self.assertTrue(signin.context['user'].is_active)
-        #self.assertEqual(str(signin.context['user']), USERNAME)
-        print "user = {}".format(signin.context['user'])
         # attempt to sign in with client login method
-        login = self.client.login(username=USERNAME, password=PASSWORD)
-        print login
-        #self.assertTrue(login)
-        home = self.client.get(reverse('home'))
-        print "home page content:"
-        print home['location']
-        print "{}".format(home.content)
+        login = self.client.login(
+            username=self.username, password=self.password
+        )
+        self.assertTrue(login)
+        response = self.client.get(earl)
+        self.assertEqual(response.status_code, 200)
+        #print "{}".format(home.content)
+
+    def test_most_recent_term(self):
+        print "\n"
+        print "Most Recent Term Test"
+        print D
+        earl = reverse('most_recent_term')
+        print earl
+        # get page
+        response = self.client.get(earl, follow=True)
+        print "redirect chain".format(response.redirect_chain)
+        print "status code: {}".format(response.status_code)
+        self.assertEqual(response.status_code, 200)
+
+        # attempt to sign in with client login method
+        login = self.client.login(
+            username=self.username, password=self.password
+        )
+        self.assertTrue(login)
+        response = self.client.get(earl)
+        self.assertEqual(response.status_code, 200)
+
+        sid = settings.TEST_STUDENT_ID
+        student = self.client.post(
+            earl, {'student_number': sid}
+        )
+
+        print "status code: {}".format(student.status_code)
+        print "user ID: {}".format(student.context['user'].id)
+        print "rendered form:\n"
+        print student.context['form']
+        print "student data:\n"
+        print student.context['student']
+
+        #session = self.client.session
+        #print session
+
+        self.assertEqual(student.context['student'][0].id, sid)
