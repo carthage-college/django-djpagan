cb5b77b920bb9a215398df8b7f38bb57b41cf1cb
plungerman <plungerman@gmail.com>
2019-03-14 16:43:46 -0500

    updates from migration to ubuntu 18.04 hygiea server

--- djpagan/wsgi_default.py
+++ djpagan/wsgi_default.py
@@ -5,15 +5,13 @@ import signal
 import sys
 
 # python
-sys.path.append('/usr/lib/python2.7/')
-sys.path.append('/usr/lib/python2.7/dist-packages/')
-sys.path.append('/usr/local/lib/python2.7/dist-packages/')
-sys.path.append('/data2/django_1.11/')
-sys.path.append('/data2/django_projects/')
-sys.path.append('/data2/django_third/')
+sys.path.append('/data2/python_venv/2.7/djpagan/lib/python2.7/')
+sys.path.append('/data2/python_venv/2.7/djpagan/lib/python2.7/site-packages/')
+sys.path.append('/data2/python_venv/2.7/djpagan/lib/django_projects/')
+sys.path.append('/data2/python_venv/2.7/djpagan/lib/django-djpagan/')
 # django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djpagan.settings')
-os.environ.setdefault('PYTHON_EGG_CACHE', '/var/cache/python/.python-eggs')
+os.environ.setdefault('PYTHON_EGG_CACHE', '')
 os.environ.setdefault('TZ', 'America/Chicago')
 # informix
 os.environ['INFORMIXSERVER'] = ''
@@ -22,8 +20,8 @@ os.environ['INFORMIXDIR'] = ''
 os.environ['ODBCINI'] = ''
 os.environ['ONCONFIG'] = ''
 os.environ['INFORMIXSQLHOSTS'] = ''
-os.environ['LD_LIBRARY_PATH'] = ''
-os.environ['LD_RUN_PATH'] = os.environ['LD_LIBRARY_PATH']
+os.environ['LD_LIBRARY_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:/usr/lib/apache2/modules:$INFORMIXDIR/lib/cli'
+os.environ['LD_RUN_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:/usr/lib/apache2/modules'
 # wsgi
 from django.core.wsgi import get_wsgi_application
 
@@ -38,4 +36,3 @@ except Exception:
         os.kill(os.getpid(), signal.SIGINT)
         time.sleep(2.5)
     exit(-1)
-
