641b652a63e7e6af41f946959d10ce9dac8e098f
plungerman <plungerman@gmail.com>
2022-04-26 10:50:31 -0500

    updates to meal-plan app to deal with faulty scans that send bunk IDs to the form

--- djpagan/forage/views.py
+++ djpagan/forage/views.py
@@ -30,6 +30,7 @@ def mealplan(request):
     cd = None
     mealplan = None
     level = None
+    student = None
     if request.method == 'POST':
         form = MealPlanForm(
             request.POST,
@@ -39,54 +40,55 @@ def mealplan(request):
             cd = form.cleaned_data
             # remove non-numeric characters from string
             cid = re.sub('[^0-9]', '', cd['cid'])
-            sql = """
-                SELECT
-                    TRIM(NVL(meal_plan_type, '')) as meal_plan,
-                    TRIM(cvid_rec.ldap_name) as username,
-                    (TRIM(cvid_rec.ldap_name) || '@carthage.edu') AS email,
-                    id_rec.lastname, id_rec.firstname
-                FROM
-                    id_rec
-                LEFT JOIN
-                    cvid_rec
-                ON
-                    id_rec.id = cvid_rec.cx_id
-                LEFT JOIN
-                    stu_serv_rec
-                ON
-                    id_rec.id = stu_serv_rec.id
-                WHERE
-                    stu_serv_rec.yr = 2022
-                AND
-                    stu_serv_rec.sess = 'RC'
-                AND id_rec.id = {0}
-            """.format(cid)
-            with get_connection(settings.INFORMIX_ODBC) as connection:
-                student = xsql(sql, connection, key=settings.INFORMIX_DEBUG).fetchone()
-                if student:
-                    status = True
-                    try:
-                        level = settings.MEAL_PLANS[student.meal_plan]
-                    except Exception:
-                        level = student.meal_plan
-                else:
-                    status = False
-                    level = ''
-                    sql = """
-                        SELECT
-                            TRIM(cvid_rec.ldap_name) as username,
-                            (TRIM(cvid_rec.ldap_name) || '@carthage.edu') AS email,
-                            id_rec.lastname, id_rec.firstname
-                        FROM
-                            id_rec
-                        LEFT JOIN
-                            cvid_rec
-                        ON
-                            id_rec.id = cvid_rec.cx_id
-                        WHERE
-                            id_rec.id = {0}
-                    """.format(cid)
+            if cid:
+                sql = """
+                    SELECT
+                        TRIM(NVL(meal_plan_type, '')) as meal_plan,
+                        TRIM(cvid_rec.ldap_name) as username,
+                        (TRIM(cvid_rec.ldap_name) || '@carthage.edu') AS email,
+                        id_rec.lastname, id_rec.firstname
+                    FROM
+                        id_rec
+                    LEFT JOIN
+                        cvid_rec
+                    ON
+                        id_rec.id = cvid_rec.cx_id
+                    LEFT JOIN
+                        stu_serv_rec
+                    ON
+                        id_rec.id = stu_serv_rec.id
+                    WHERE
+                        stu_serv_rec.yr = 2022
+                    AND
+                        stu_serv_rec.sess = 'RC'
+                    AND id_rec.id = {0}
+                """.format(cid)
+                with get_connection(settings.INFORMIX_ODBC) as connection:
                     student = xsql(sql, connection, key=settings.INFORMIX_DEBUG).fetchone()
+                    if student:
+                        status = True
+                        try:
+                            level = settings.MEAL_PLANS[student.meal_plan]
+                        except Exception:
+                            level = student.meal_plan
+                    else:
+                        status = False
+                        level = ''
+                        sql = """
+                            SELECT
+                                TRIM(cvid_rec.ldap_name) as username,
+                                (TRIM(cvid_rec.ldap_name) || '@carthage.edu') AS email,
+                                id_rec.lastname, id_rec.firstname
+                            FROM
+                                id_rec
+                            LEFT JOIN
+                                cvid_rec
+                            ON
+                                id_rec.id = cvid_rec.cx_id
+                            WHERE
+                                id_rec.id = {0}
+                        """.format(cid)
+                        student = xsql(sql, connection, key=settings.INFORMIX_DEBUG).fetchone()
             if student:
                 # check for an exisiting user
                 user = User.objects.filter(pk=cid).first()
