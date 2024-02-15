ce969005d047f890255fa21a1889223a340bd1c3
plungerman <plungerman@gmail.com>
2024-02-15 12:16:33 -0600

    updates for migration from python 3.8 to 3.10

--- djpagan/wsgi_default.py
+++ djpagan/wsgi_default.py
@@ -1,11 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""WSGI configuration."""
+
 import os
 import sys
 
+
 # python
-sys.path.append('')
-sys.path.append('')
+sys.path.append('/d2/python_venv/3.10/djlamantin/lib/python3.10/')
+sys.path.append('/d2/python_venv/3.10/djlamantin/lib/python3.10/site-packages/')
 # django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djpagan.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djpagan.settings.shell')
 os.environ.setdefault('PYTHON_EGG_CACHE', '/var/cache/python/.python-eggs')
 os.environ.setdefault('TZ', 'America/Chicago')
 # informix
